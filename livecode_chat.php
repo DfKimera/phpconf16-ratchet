532caa40dd51626c2f173bdd92d72a106a7e89ac
Aryel Tupinambá <aryel.tupinamba@lqdi.net>
2016-12-10 00:35:31 -0200

    Added live code boilerplate

---
+++ livecode_chat.php
@@ -0,0 +1,6 @@
+<?php
+include("vendor/autoload.php");
+
+// COLA :D
+// Class < MessageComponentInterface: onOpen, onMessage, onClose; implement broadcast
+// Server: IoServer::factory, HttpServer, WsServer, Class; $server->run
